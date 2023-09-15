28309de6f8da94b46a5957b70a5c1e4c41bebd3c
Eduardo Verissimo <84536168+EduardoVeri@users.noreply.github.com>
2023-09-14 22:55:24 -0300

    Working serial version

--- teste_edu.c
+++ teste_edu.c
@@ -28,7 +28,7 @@
 #include <unistd.h>
 
 #define board_size 2048
-#define number_of_iterations 500
+#define number_of_iterations 2000
 
 float** allocate_board();
 void free_board(float **grid);
@@ -44,6 +44,7 @@ int main(int argc, char **argv)
     grid = allocate_board();
     newgrid = allocate_board();
     initialize_board(grid);
+
     execute_iterations(grid, newgrid, number_of_iterations);
     compute_live_cells(grid);
 
@@ -94,23 +95,23 @@ void initialize_board(float **grid)
 
     // initialize the board from position (1,1) with glider pattern
     // and a R-pentomino pattern in (10, 30)
-    grid[1+1][2+1] = 1.0;
-    grid[2+1][3+1] = 1.0;
-    grid[3+1][1+1] = 1.0;
-    grid[3+1][2+1] = 1.0;
-    grid[3+1][3+1] = 1.0;
+    grid[1][2] = 1.0;
+    grid[2][3] = 1.0;
+    grid[3][1] = 1.0;
+    grid[3][2] = 1.0;
+    grid[3][3] = 1.0;
 
-    grid[10+1][31+1] = 1.0;
-    grid[10+1][32+1] = 1.0;
-    grid[11+1][30+1] = 1.0;
-    grid[11+1][31+1] = 1.0;
-    grid[12+1][31+1] = 1.0;
+    grid[10][31] = 1.0;
+    grid[10][32] = 1.0;
+    grid[11][30] = 1.0;
+    grid[11][31] = 1.0;
+    grid[12][31] = 1.0;
 
 }
 
 int get_neighbors(float **grid, int i, int j){
     int number_of_neighbors = 0;
-    
+
     for(int k = i - 1; k <= i + 1; k++)
     {
         for(int l = j - 1; l <= j + 1; l++)
@@ -123,21 +124,26 @@ int get_neighbors(float **grid, int i, int j){
             {
                 continue;
             }
-            if(k == -1){
+
+            if(k == -1)
+            {
                 k_aux = board_size - 1;
             }
+            else if (k == board_size)
+            {
+                k_aux = 0;
+            }
+            
             if(l == -1){
                 l_aux = board_size - 1;
             }
-            if(k == board_size){
-                k_aux = 0;
-            }
-            if(l == board_size){
+            else if(l == board_size)
+            {
                 l_aux = 0;
             }
 
             if(grid[k_aux][l_aux] > 0.0)
-            {
+            {   
                 number_of_neighbors++;
             }
         }
@@ -157,40 +163,35 @@ float average_neighbors_value(float** grid, int i,  int j){
             int k_aux = k;
             int l_aux = l;
 
-            if(k == i && l == j)
+            if(k == -1)
             {
-                continue;
-            }
-            if(k == -1){
                 k_aux = board_size - 1;
             }
+            else if (k == board_size){
+                k_aux = 0;
+            }
+
             if(l == -1){
                 l_aux = board_size - 1;
             }
-            if(k == board_size){
-                k_aux = 0;
-            }
-            if(l == board_size){
+            else if(l == board_size){
                 l_aux = 0;
             }
-
-            if(grid[k_aux][l_aux] > 0.0)
-            {
-                average += grid[k_aux][l_aux];
-            }
+            
+            average += (float)grid[k_aux][l_aux];  
         }
     }
-    //printf("average: %f\n", average / 8.0);
-    return average / 8.0;
+
+    return (float)average / (float)8.0;
 }
 
 void execute_iterations(float **grid , float **newgrid, int iterations)
 {
     for(int i = 0; i < iterations; i++)
     {   
-        for(int j = 1; j < board_size; j++)
+        for(int j = 0; j < board_size; j++)
         {
-            for(int k = 1; k < board_size; k++)
+            for(int k = 0; k < board_size; k++)
             {
                 
                 // get neighbors
@@ -201,7 +202,7 @@ void execute_iterations(float **grid , float **newgrid, int iterations)
                 {
                     if(number_of_neighbors == 2 || number_of_neighbors == 3)
                     {
-                        newgrid[j][k] = grid[j][k];
+                        newgrid[j][k] = 1;
                     }
                     else
                     {
@@ -221,6 +222,7 @@ void execute_iterations(float **grid , float **newgrid, int iterations)
                 }   
             }
         }
+        
         //printf("Before copy\n");
         // copy newgrid to grid
         for(int j = 0; j < board_size; j++)
@@ -231,7 +233,8 @@ void execute_iterations(float **grid , float **newgrid, int iterations)
             }
         }
         //printf("After copy\n");
-        //compute_live_cells(grid);
+        printf("iteration: %d ", i);
+        compute_live_cells(grid);
         
     }
     compute_live_cells(grid);
@@ -240,9 +243,9 @@ void execute_iterations(float **grid , float **newgrid, int iterations)
 void compute_live_cells(float **grid)
 {
     int live_cells = 0;
-    for (int i = 1; i < board_size - 1; i++)
+    for (int i = 0; i < board_size; i++)
     {
-        for (int j = 1; j < board_size - 1; j++)
+        for (int j = 0; j < board_size; j++)
         {
             if (grid[i][j] > 0.0)
             {
