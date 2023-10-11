7e3a1d2d06fe9281a2ec7d972d489d805fc2a3c7
Marco Antonio Coral dos Santos <110304504+marcoantoniocoraldossantos@users.noreply.github.com>
2023-10-11 09:20:05 -0300

    Update concorrencia.c

--- Atividade 2/Item 1/concorrencia.c
+++ Atividade 2/Item 1/concorrencia.c
@@ -1,56 +1,46 @@
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
 
-#define MAX_THREADS 100
+// variaveis globais
+int sum = 0;
+int target_sum = 100;
 
-int SOMA = 0;
-int thread_id_counter = 0;
+// funcoes do servidor e cliente
+void critical_section(int id);
 
-void *incrementSoma(void *arg) {
-    int iterations = *((int *)arg);
-    int thread_id;
-
-    // Obter um ID de thread sequencial e incrementar o contador
-    thread_id = thread_id_counter++;
-    
-    for (int i = 0; i < iterations; i++) {
-        SOMA++;
-        printf("thread %d \t| incremented: %d\n", thread_id, SOMA);
-    }
-
-    pthread_exit(NULL);
-}
-
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-        printf("Uso: %s <número de threads> <número máximo de iterações>\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Uso: %s <N>\n", argv[0]);
         return 1;
     }
 
     int num_threads = atoi(argv[1]);
-    int iterations_per_thread = atoi(argv[2]);
 
-    if (num_threads > MAX_THREADS) {
-        printf("O número de threads excede o máximo permitido (%d)\n", MAX_THREADS);
-        return 1;
-    }
-
-    pthread_t threads[MAX_THREADS];
-
-    for (int i = 0; i < num_threads; i++) {
-        int rc = pthread_create(&threads[i], NULL, incrementSoma, (void *)&iterations_per_thread);
-        if (rc) {
-            printf("Erro ao criar a thread %d; código de retorno: %d\n", i, rc);
-            return -1;
+    #pragma omp parallel num_threads(num_threads)
+    {
+        int id = omp_get_thread_num();
+        
+        while (sum < target_sum)
+        {
+            critical_section(id);
         }
     }
 
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    printf("\nfinal sum: %d\n", SOMA);
-
     return 0;
+}
+
+// secao critica
+void critical_section(int id)
+{
+    if (sum >= target_sum)
+        return;
+
+    printf("thread %d: critical section\n", id);
+    sleep(1);
+    sum += 1;
+    printf("thread %d: sum = %d\n", id, sum);
 }
