270625bdcbc2fea1d286f54acbdb95c7012085d4
marcoantoniocoraldossantos <marcoantoniocoraldossantos@gmail.com>
2023-10-11 09:05:52 -0300

    last changes

--- Atividade 2/Item 3/ex3/TrafficController.java
+++ Atividade 2/Item 3/ex3/TrafficController.java
@@ -1,44 +1,28 @@
+
 import java.util.concurrent.Semaphore;
 
 public class TrafficController {
-
-    private int carsOnBridge = 0;
-    private Semaphore leftSemaphore = new Semaphore(1);  // Semaphore para carros da esquerda
-    private Semaphore rightSemaphore = new Semaphore(1); // Semaphore para carros da direita
+    private Semaphore semaphore = new Semaphore(1);
 
     public void enterLeft() {
         try {
-            leftSemaphore.acquire(); // Adquire o semaphore da esquerda
-            while (carsOnBridge > 0) {
-                // Aguarda até que a ponte esteja vazia
-                Thread.sleep(100); // Pode adicionar um tempo de espera aqui para evitar um loop ocupado
-            }
-            carsOnBridge++;
+	    semaphore.acquire();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
     }
-
     public void enterRight() {
         try {
-            rightSemaphore.acquire(); // Adquire o semaphore da direita
-            while (carsOnBridge > 0) {
-                // Aguarda até que a ponte esteja vazia
-                Thread.sleep(100); // Pode adicionar um tempo de espera aqui para evitar um loop ocupado
-            }
-            carsOnBridge++;
+	    semaphore.acquire();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-        }
+        }     
     }
-
     public void leaveLeft() {
-        carsOnBridge--;
-        leftSemaphore.release(); // Libera o semaphore da esquerda
+        semaphore.release();
+    }
+    public void leaveRight() {
+        semaphore.release();
     }
 
-    public void leaveRight() {
-        carsOnBridge--;
-        rightSemaphore.release(); // Libera o semaphore da direita
-    }
 }
